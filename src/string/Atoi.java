e2bda3b048d446bed8bb67aae7438ad344ca8706
jian.han3 <jian.han3@hp.com>
2014-12-15 19:49:11 +0800

    leetcode: atoi tag: String

---
+++ src/string/Atoi.java
@@ -0,0 +1,46 @@
+package string;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by jiahan on 12/15/14.
+ */
+public class Atoi {
+    @Test
+    public void atoi() {
+        String a = "2147483648";
+        int b = atoi(a);
+        assertEquals("failure - atoi  of '2147483648' not right",
+                2147483647, b);
+    }
+    private static final int maxDiv10 = Integer.MAX_VALUE / 10;
+    public int atoi(String str) {
+        if (str == null || str.length() == 0) {
+            return 0;
+        }
+        int i = 0;
+        int n = str.length();
+        while (i < n && str.charAt(i) == ' ') {
+            i++;
+        }
+        int sign = 1;
+        if (i < n && str.charAt(i) == '+') {
+            i++;
+        } else if (i < n && str.charAt(i) == '-') {
+            sign = -1;
+            i++;
+        }
+        int num = 0;
+        while (i < n && Character.isDigit(str.charAt(i))) {
+            int digit = Character.getNumericValue(str.charAt(i));
+            if (num > maxDiv10 || (num == maxDiv10 && digit >= 8)) {
+                return sign == 1 ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+            }
+            num = num * 10 + digit;
+            i++;
+        }
+        return sign * num;
+    }
+}
