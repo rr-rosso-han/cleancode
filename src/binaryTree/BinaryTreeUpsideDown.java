3869a6b1483a29321a5948100079972f0827e2b2
jian.han3@gmail.com <jian.han3@hp.com>
2015-01-02 00:56:33 +0800

    single number

--- src/binaryTree/BinaryTreeUpsideDown.java
+++ src/binaryTree/BinaryTreeUpsideDown.java
@@ -4,4 +4,37 @@ package binaryTree;
  * Created by jiahan on 1/1/15.
  */
 public class BinaryTreeUpsideDown {
+
+    public TreeNode UpsideDownBinaryTree(TreeNode root) {
+        if (root == null) {
+            return null;
+        }
+        TreeNode p = root;
+        TreeNode parent = null;
+        TreeNode parentRight = null;
+
+        while (p != null) {
+            TreeNode left = p.left;
+            p.left = parentRight;
+            parentRight = p.right;
+            p.right = parent;
+            parent = p;
+            p = left;
+        }
+        return parent;
+    }
+
+    public TreeNode UpsideDownBinaryTree2(TreeNode root) {
+        return dfsBottomUp(root, null);
+    }
+
+    private TreeNode dfsBottomUp(TreeNode p, TreeNode parent) {
+        if (p == null) {
+            return parent;
+        }
+        TreeNode root = dfsBottomUp(p.left, p);
+        p.left = (parent == null) ? parent : parent.right;
+        p.right = parent;
+        return root;
+    }
 }
--- src/bitmanipulation/SingleNumber.java
+++ src/bitmanipulation/SingleNumber.java
@@ -1,7 +1,30 @@
 package bitmanipulation;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Created by jiahan on 1/1/15.
  */
 public class SingleNumber {
+    public int singleNumber(int[] A) {
+        if (A == null || A.length == 0) {
+            throw new IllegalArgumentException("Invalid Input");
+        }
+        Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+        for (int i = 0; i < A.length - 1; i++) {
+            if (map.containsKey(A[i])) {
+                map.put(A[i], map.get(A[i]) + 1);
+            }
+            map.put(A[i], 1);
+        }
+
+        for (int i = 0; i < A.length - 1; i++) {
+            if (map.get(A[i]) == 1) {
+                return A[i];
+            }
+        }
+        throw new IllegalArgumentException("Invalid Input");
+    }
 }
